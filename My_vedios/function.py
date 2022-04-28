48265cb5de7284a8f6c0d6d88b53cdd61cfe28e5
Gillott <96903386+Gillott@users.noreply.github.com>
2022-04-28 17:07:06 +0800

    Update function.py

--- My_vedios/function.py
+++ My_vedios/function.py
@@ -1,3 +1,11 @@
+"""
+这里存放着一些功能函数（基本都直接把源码复制了过来），
+1.获取直线交点的函数 get_intersect()
+来自MK官方文档：https://docs.manim.org.cn/documentation/utils/space_ops.html
+2.获取圆外一点与圆切点的函数 get_tangent_line()
+来自@bilibili_UP主josepa的专栏:https://b23.tv/2mY0kDx
+"""
+
 from manim import*
 
 def get_intersect(line1, line2, parallel):
@@ -22,46 +30,6 @@ def get_intersect(line1, line2, parallel):
         y = (a1 * c2 - a2 * c1) / determinant
         return x * RIGHT + y * UP
 
-
-import math
-
-def easeOutBounce(t):
-    if t < 1 / 2.75:
-        return 7.5625 * t * t
-    elif t < 2 / 2.75:
-        c = t - 1.5/2.75
-        return 7.5625 * c * c + 0.75
-    elif t < 2.5 / 2.75:
-        c = t - 2.25 / 2.75
-        return 7.5625 * c * c + .9375
-    else:
-        c = t - 2.625 / 2.75
-        return 7.5625 * c * c + .984375
-
-def easeInBounce(t):
-    return 1 - easeOutBounce(1 - t)
-
-def easeInOutBounce(t):
-    if t < 0.5:
-        return easeInBounce(2 * t)
-    else:
-        return easeOutBounce(2 * t - 1)
-    
-
-
-def easeOutElastic(t):
-    s, a = 1.70158, 1
-    
-    if t == 0: return 0
-    if t == 1: return 1
-
-    p = 0.3
-    if a < 1:
-        a, s = 1, p / 4
-    else:
-        s = p / (2 * math.pi) * math.asin(1 / a)
-
-    return a * pow(2, -10 * t) * math.sin((t - s) * (2 * math.pi) / p) + 1
 
 
 
