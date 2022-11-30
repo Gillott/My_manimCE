3c14c6c8eb97a4acf2007bcbe9dc38f7c4efb8be
Gillott <96903386+Gillott@users.noreply.github.com>
2022-11-30 19:49:14 +0800

    Update func_math.py

--- My_vedios/math/func_math.py
+++ My_vedios/math/func_math.py
@@ -19,15 +19,14 @@ class ThreeSVG(VGroup):
         svg3 = SVGMobject("D:\\SVG\\favo.svg").set_opacity(1).set_color(PINK)
         VGroup(svg1,svg2,svg3).arrange_submobjects(buff=1.3)
         self.add(svg1,svg2,svg3)
-        
-class ThreeSVG(VGroup):
-    """一键三连"""
+class MurCat(VGroup):
+    """mur猫表情包"""
     def __init__(self,**kwargs):
         VGroup.__init__(self,**kwargs)
-        svg1 = SVGMobject("D:\\SVG\\good.svg").set_opacity(1).set_color(RED)
-        svg2 = SVGMobject("D:\\SVG\\coin.svg").set_opacity(1).set_color(ORANGE)
-        svg3 = SVGMobject("D:\\SVG\\favo.svg").set_opacity(1).set_color(PINK)
-        self.add(svg1,svg2,svg3)
+        svg = SVGMobject("D:\\SVG\\murcat.svg").set_color(WHITE)
+        self.add(svg)
+#####################################################
+
         
 def get_intersect(line1, line2, parallel):
     p1, p2 = line1.get_start_and_end()
